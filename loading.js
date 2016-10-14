537d154e681318e7de9d26c126edc08ab0173d5e
关宁 guanning (99999) <guanning@tuniu.com>
2016-10-14 10:16:32 +0800

    first commit

---
+++ loading.js
@@ -0,0 +1,52 @@
+// define(function(require) {
+    // var $ = require('jquery')
+    var Loading = function(CFG) {
+        this.cfg = {
+            target: CFG.target || null,
+            text: '请稍微等候一下，牛牛正在努力加载中'
+        }
+        this.loadingTpl = '<div class="loading-content">' + 
+                              '<img src="http://img4.tuniucdn.com/img/20160315/hotel_2016/common/loading_new.gif"></img>' + 
+                              '<span class="loading-text">' + this.cfg.text + '</span>' + 
+                          '</div>';
+    } 
+    Loading.prototype = {
+    	renderUI: function() {
+            $(this.cfg.target).append($(this.loadingTpl));
+    	},
+    	syncUI: function() {	
+    		$(this.cfg.target).css({
+                'position': 'relative',
+                'text-align': 'center'
+    		});
+    		$(this.loadingTpl).css({
+                'position': 'absolute',
+                'top': '50%',
+                'left': '50%',
+                'transform': 'translate(-50% -50%)' 
+    		});
+    		$(this.loadingTpl).find('img').css({
+    			'display': 'block',
+                'width': '120px',
+                'height': '120px'
+    		}); 
+    		$('.loading-text').css({
+                'color': 'green',
+                'display': 'block',
+                'margin-top': '20px',
+                'font-size': '12px'
+    		});
+    	},
+    	render: function() {
+            this.renderUI();
+    	    this.syncUI();
+        },
+        start: function() {
+        	this.render();
+        },
+        destroy: function() {
+            $(this.cfg.target).remove();
+        }
+    }
+    // return Loading;
+// });
